a6f1810e6e4504cc835315886eb2654abc7ac3e1
Rashmi-Mathpati <mathpatirashmi1995@gmail.com>
2022-06-07 13:43:49 +0530

    code is committed

---
+++ src/test/java/com/employee/employee_application/AppTest.java
@@ -0,0 +1,18 @@
+package com.employee.employee_application;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
